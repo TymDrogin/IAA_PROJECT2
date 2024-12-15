5a0fd81b93f387676a17e660542566f57e416123
Tym Drogin <tim.drogin@gmail.com>
2024-12-15 18:36:12 +0000

    Everything is fixed

--- src/algorithm.c
+++ src/algorithm.c
@@ -25,7 +25,7 @@ void repair_solution(int* sol, const Data* data) {
 
 	int deviation = solution_target_diff(sol, data);
 	if (deviation > 0) {
-		for (int i = 0; i < data->coin_types_n; i++) {
+		for (int i = data->coin_types_n - 1; i >= 0; i--) {
 			int coins_to_remove = deviation / data->coin_values_in_cents[i];
 			if (sol[i] >= coins_to_remove) {
 				sol[i] -= coins_to_remove;
@@ -38,6 +38,7 @@ void repair_solution(int* sol, const Data* data) {
 			}
 		}
 	}
+	deviation = solution_target_diff(sol, data);
 	if (deviation < 0) {
 		for (int i = data->coin_types_n - 1; i >= 0; i--) {
 			int coins_to_add = abs(deviation / data->coin_values_in_cents[i]);
@@ -54,52 +55,60 @@ void repair_solution(int* sol, const Data* data) {
 
 
 
-
-
-
 int solution_cost(const int* solution, const Data* data) {
 	// Penalize invalid solutions
 	if (!is_valid_solution(solution, data)) return INT_MAX;
 	return num_of_coins_used(solution, data);
 }
-int* generate_neighbor(const int* sol, const Data* data) {
+int* generate_neighbor(const int* sol, const Data* data, int num_pos_to_mutate) {
+	// Array to hold the indices
+	int indices[data->coin_types_n];
+	for (int i = 0; i < data->coin_types_n; i++) {
+		indices[i] = i;
+	}
+
+	// Shuffle the indices using Fisher-Yates
+	for (int i = data->coin_types_n - 1; i > 0; i--) {
+		int j = rand() % (i + 1);
+		// Swap indices[i] and indices[j]
+		int temp = indices[i];
+		indices[i] = indices[j];
+		indices[j] = temp;
+	}
+
+	// Select the first 'select_count' indices
+	if (num_pos_to_mutate > data->coin_types_n) {
+		num_pos_to_mutate = data->coin_types_n;
+	}
+	int pos_to_mutate[num_pos_to_mutate];
+	for (int i = 0; i < num_pos_to_mutate; i++) {
+		pos_to_mutate[i] = indices[i];
+	}
+	// Mutate solution
 	int* new_sol = (int*)malloc(sizeof(int) * data->coin_types_n);
 	memcpy(new_sol, sol, data->coin_types_n * sizeof(int));
 
-	// Random positions to mutate
-	int pos1, pos2, pos3, pos4;
-	pos1 = rand() % data->coin_types_n;
-	// Make sure positions are different
-	do {
-		pos2 = rand() % data->coin_types_n;
-	} while (pos2 == pos1 || new_sol[pos2] <= 0);
-		
-	pos3 = rand() % data->coin_types_n;
-	// Make sure positions are different
-	do {
-		pos4 = rand() % data->coin_types_n;
-	} while (pos4 == pos3 || new_sol[pos4] <= 0);
 
-	new_sol[pos1]++;
-	new_sol[pos2]--;
-	new_sol[pos3]++;
-	new_sol[pos4]--;
-	for (int i = 0; i < data->coin_types_n; i++) {
-		if (new_sol[i] < 0) {
-			new_sol[i] = 0;
+	for (int i = 0; i < num_pos_to_mutate; i++) {
+		if (i % 2) {
+			new_sol[pos_to_mutate[i]]++;
+		} else {
+			if (new_sol[pos_to_mutate[i]] - 1 >= 0) {
+				new_sol[pos_to_mutate[i]]--;
+			} else {
+				new_sol[pos_to_mutate[i]] = 0;
+			}
 		}
 	}
 
-	if (!is_valid_solution(new_sol, data)) {
-		repair_solution(new_sol, data);
-	}
+	repair_solution(new_sol, data);
 	return new_sol;
 }
 
 int hill_climbing(int* initial_solution, Data* data, int num_iter) {
 	int cost = solution_cost(initial_solution, data);
 	for (int i = 0; i < num_iter; i++) {
-		int* new_sol = generate_neighbor(initial_solution, data);
+		int* new_sol = generate_neighbor(initial_solution, data, 4);
 
 		int costNeighbor = solution_cost(new_sol, data);
 		if (costNeighbor <= cost) {
